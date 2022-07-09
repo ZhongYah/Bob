65fa59e5df9204b55e2cf0a87068f901545810d9
ZhongYah <s763460336@gmail.com>
2022-07-09 17:45:32 +0800

    char.13

---
+++ src/main/java/com/yah/student/StudentRunner.java
@@ -0,0 +1,8 @@
+package com.yah.student;
+
+public class StudentRunner {
+    public static void main(String[] args) {
+         Student stu = new Student("Hank", 60, 77);
+         stu.print();
+    }
+}
