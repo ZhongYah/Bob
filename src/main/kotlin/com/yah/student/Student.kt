a4b283d2d55b45de6e39526fa0d01196a29af5d2
ZhongYah <s763460336@gmail.com>
2022-07-14 23:49:08 +0800

    char.16

--- src/main/kotlin/com/yah/student/Student.kt
+++ src/main/kotlin/com/yah/student/Student.kt
@@ -3,6 +3,14 @@ package com.yah
 import java.util.Scanner
 
 fun main(args: Array<String>) {
+  //  userln()
+    val stu = Student("Hank", 77, 99)
+    stu.print()
+    val test = 123
+    println("Try is : $test")
+    println("Hight score: ${stu.highest()}")}
+
+private fun userln() {
     val scanner = Scanner(System.`in`)
     print("Please enter student's name:")
     //var name = scanner.next()
@@ -14,12 +22,30 @@ fun main(args: Array<String>) {
     val stu = Student(name, english, math)
     stu.print()
     stu.nameCheck()
+
 }
 
 class Student (var name: String?, var english: Int, var math: Int) {
     fun print() {
         println(name + "\t" + english + "\t" + math + "\t" +
                 (english + math) / 2)
+    }
+
+    fun highest() : Int {
+        var max = if (english > math) {
+            println("english")
+            english
+        } else {
+            println("math")
+            math
+        }
+
+       /* if (english > math) {
+            max = english
+        } else {
+            max = math
+        }*/
+        return max
     }
 
     fun nameCheck() {
